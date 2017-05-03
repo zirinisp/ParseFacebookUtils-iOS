b90324880545607b506433075e570db8e5f00143
Pantelis Zirinis <zirinisp@Panteliss-MacBook-Pro.local>
2017-05-02 22:06:52 -0500

    Added dynamic framework for carthage support

---
+++ ParseFacebookUtilsV4-iOS-Dynamic/ParseFacebookUtilsV4-iOS-Dynamic.h
@@ -0,0 +1,19 @@
+//
+//  ParseFacebookUtilsV4-iOS-Dynamic.h
+//  ParseFacebookUtilsV4-iOS-Dynamic
+//
+//  Created by Pantelis Zirinis on 02/05/2017.
+//  Copyright © 2017 Parse, LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ParseFacebookUtilsV4-iOS-Dynamic.
+FOUNDATION_EXPORT double ParseFacebookUtilsV4_iOS_DynamicVersionNumber;
+
+//! Project version string for ParseFacebookUtilsV4-iOS-Dynamic.
+FOUNDATION_EXPORT const unsigned char ParseFacebookUtilsV4_iOS_DynamicVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ParseFacebookUtilsV4_iOS_Dynamic/PublicHeader.h>
+
+
